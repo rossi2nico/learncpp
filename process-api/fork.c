abc2e207703f414f7a703c31a1ebc6c6f3c9e5db
rossi2nico <rossi2nick@gmail.com>
2026-01-01 11:34:08 -0500

    process-api folder

---
+++ process-api/fork.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+	printf("hello (PID: %d)\n", (int) getpid());
+	
+	int rc = fork();
+	if (rc < 0) {
+		printf("fork failed\n");
+	} 
+	else if (rc == 0) {
+		printf("child (PID: %d)\n", (int) getpid());
+	}
+	else {
+		int rc_wait = wait(NULL)
+		printf("parent of %d (rc_wait: %d) (PID: %d)\n", rc, rc_wait, (int) getpid());
+	}
+
+	return 0;
+}
